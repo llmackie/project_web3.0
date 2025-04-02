e74d1e08fb450a38117258f0968a0a6151823f61
Paul Reginald Omboko <121097269+llmackie@users.noreply.github.com>
2025-04-02 07:20:26 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import { TransactionsProvider } from "./context/TransactionContext";
 import "./index.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <TransactionsProvider>
     <App />
-  </TransactionsProvider>,
-  document.getElementById("root"),
+  </TransactionsProvider>
 );
